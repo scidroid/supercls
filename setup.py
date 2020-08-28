b07f567532dbcf7ef6b7b3b4fc1ebb11b5687078
SciDroid <scidroidgames@gmail.com>
2020-08-28 11:39:06 -0500

    Library add to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'supercls',
+  packages = ['supercls'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Supercls is a Python package that allows you to easily make a clear on the console that works on most Python-compatible platforms by just typing a command.',
+  author = 'Scidroid',
+  author_email = 'scidroidgames@gmail.com',
+  url = 'https://github.com/scidroid/supercls', # use the URL to the github repo
+  download_url = 'https://github.com/scidroid/supercls/tarball/0.1',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
