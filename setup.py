605092062b334cbc99bd9490608d0d8d79f1f2e3
SciDroid <scidroid@scidroid.me>
2021-08-24 12:56:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'supercls',
   packages = ['supercls'], # this must be the same as the name above
-  version = '0.1',
+  version = '1.0',
   description = 'Supercls is a Python package that allows you to easily make a clear on the console that works on most Python-compatible platforms by just typing a command.',
   author = 'Scidroid',
-  author_email = 'scidroidgames@gmail.com',
+  author_email = 'scidroid@scidroid.me',
   url = 'https://github.com/scidroid/supercls', # use the URL to the github repo
-  download_url = 'https://github.com/scidroid/supercls/tarball/0.1',
+  download_url = 'https://github.com/scidroid/supercls/tarball/1.0',
   keywords = ['testing', 'logging', 'example'],
   classifiers = [],
 )
